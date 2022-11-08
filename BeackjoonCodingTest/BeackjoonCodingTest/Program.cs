3aac291a267f2ed5c92f84b5ee7321d15535c8e7
lim du hyoung <limduh2@naver.com>
2022-11-08 13:25:42 +0900

    백준코딩테스트  파일 생성
    스트링빌더 추가

--- BeackjoonCodingTest/BeackjoonCodingTest/Program.cs
+++ BeackjoonCodingTest/BeackjoonCodingTest/Program.cs
@@ -10,6 +10,30 @@ namespace BeackjoonCodingTest
     {
         static void Main(string[] args)
         {//백준 시작 스트링빌더
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+
+            int[] a = new int[t];
+            int[] b = new int[t];
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                a[i] = int.Parse(s[0]);
+                b[i] = int.Parse(s[1]);
+
+                sb.Append(a[i] + b[i] + "\n");
+            }
+            //합들이 저장 되었다가 한 번에 출력된다.
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
