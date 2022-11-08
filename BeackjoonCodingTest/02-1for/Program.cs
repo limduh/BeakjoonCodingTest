96a3c4e9024f9eaaf5146339292bec98a1ca5a4a
lim du hyoung <limduh2@naver.com>
2022-11-08 23:35:39 +0900

    for문 2개 추가
    fo문 2개 코딩해보기

---
+++ BeackjoonCodingTest/02-1for/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_1for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
